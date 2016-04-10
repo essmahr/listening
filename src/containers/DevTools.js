4ceca5ec68d555fd1418e6e2a70b25c1cb06d2e3
Scott Mahr <essmahr@gmail.com>
2016-04-09 19:57:46 -0700

    devTools collapsed by default

--- src/containers/DevTools.js
+++ src/containers/DevTools.js
@@ -4,7 +4,10 @@ import LogMonitor from 'redux-devtools-log-monitor';
 import DockMonitor from 'redux-devtools-dock-monitor';
 
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey="ctrl-h" changePositionKey="ctrl-w">
+  <DockMonitor
+    toggleVisibilityKey="ctrl-h"
+    changePositionKey="ctrl-w"
+    defaultIsVisible={false}>
     <LogMonitor />
   </DockMonitor>
 );
