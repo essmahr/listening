a3c978de768b447a9cf924b2bfa3e5f6060862c9
Scott Mahr <essmahr@gmail.com>
2016-04-17 11:34:25 -0700

    Add onscroll listener for preloading

--- src/components/ImageLoader.js
+++ src/components/ImageLoader.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import classnames from 'classnames';
+import debounce from 'lodash.debounce';
 
 class ImageLoader extends React.Component {
+  onScroll = debounce(() => {
+    this.checkVisibility()
+  }, 100);
+
   constructor(props) {
     super(props);
 
     this.state = {
       imgLoaded: this.isImgCached(),
+      imgSrc: '',
     }
   }
+
 
   onImgLoad() {
     this.setState({
@@ -24,17 +31,38 @@ class ImageLoader extends React.Component {
     // return image.complete;
   }
 
+  isInViewport() {
+    var rect = this.el.getBoundingClientRect();
+    return (rect.bottom - 60) <= (window.innerHeight || document.documentElement.clientHeight);
+  }
+
+  checkVisibility() {
+    if(this.isInViewport()) {
+      this.setState({
+        imgSrc: this.props.imgSrc,
+      });
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll);
+    this.onScroll();
+  }
+
+  componentWillUnMount() {
+    window.removeEventListener('scroll', this.onScroll);
+  }
+
   render() {
-    const classes = classnames({
-      'img-loader-container': true,
+    const classes = classnames('img-loader-container', {
       'no-image': this.props.imgSrc.length === 0,
-      [this.props.className]: (this.props.className !== undefined),
       'img-loaded': this.state.imgLoaded,
+      [this.props.className]: (this.props.className !== undefined),
     })
 
     return (
-      <div className={classes}>
-        {this.props.imgSrc.length ? <img className="img-loader-img" src={this.props.imgSrc} onLoad={this.onImgLoad.bind(this)} /> : null}
+      <div className={classes} ref={(el) => this.el = el}>
+        {this.props.imgSrc.length ? <img className="img-loader-img" src={this.state.imgSrc} onLoad={this.onImgLoad.bind(this)} /> : null}
         {this.props.children}
       </div>
     );
