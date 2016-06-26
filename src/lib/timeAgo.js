5ef3124653e9c2ca1dfe111e5890cd87fa7f8e98
Cory Gibbons <hello@corygibbons.com>
2016-06-26 03:27:15 -0700

    Show hours if time ago is greater than 60 minutes
    Also prevents strings like “1 hours ago”
    
    via http://stackoverflow.com/a/23259289

--- src/lib/timeAgo.js
+++ src/lib/timeAgo.js
@@ -1,35 +1,40 @@
 export default function(date) {
   var seconds = Math.floor((new Date() - date) / 1000);
+  var intervalType;
 
   var interval = Math.floor(seconds / 31536000);
 
-  if (interval > 1) {
-    return interval + " years ago";
+  if (interval >= 1) {
+    intervalType = "year";
+  } else {
+    interval = Math.floor(seconds / 2592000);
+    if (interval >= 1) {
+      intervalType = "month";
+    } else {
+      interval = Math.floor(seconds / 86400);
+      if (interval >= 1) {
+        intervalType = "day";
+      } else {
+        interval = Math.floor(seconds / 3600);
+        if (interval >= 1) {
+          intervalType = "hour";
+        } else {
+          interval = Math.floor(seconds / 60);
+          if (interval >= 1) {
+            intervalType = "minute";
+          } else {
+            interval = seconds;
+            intervalType = "second";
+          }
+        }
+      }
+    }
   }
 
-  interval = Math.floor(seconds / 2592000);
-
-  if (interval > 1) {
-    return interval + " months ago";
+  if (interval > 1 || interval === 0) {
+    intervalType += "s";
   }
 
-  interval = Math.floor(seconds / 86400);
+  return interval + " " + intervalType + " ago";
 
-  if (interval > 1) {
-    return interval + " days ago";
-  }
-
-  interval = Math.floor(seconds / 3600);
-
-  if (interval > 1) {
-    return interval + " hours ago";
-  }
-
-  interval = Math.floor(seconds / 60);
-
-  if (interval > 1) {
-    return interval + " minutes ago";
-  }
-
-  return Math.floor(seconds) + " seconds ago";
 }
